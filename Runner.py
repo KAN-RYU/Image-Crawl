9268166ce892cceaf1714f20bb22b789143e0eed
KAN-RYU <kanryu2048@unist.ac.kr>
2020-03-12 05:44:48 +0900

    Update Queue system

--- Runner.py
+++ Runner.py
@@ -1,15 +1,22 @@
 import Image_Downloader
+import time
 
 queueFile = open("List.txt", "r")
 download_Queue = queueFile.readlines()
 queueFile.close()
 totalNumber = len(download_Queue)
+doneList = []
+
+#첫번째 주소 불러오기
+current = download_Queue.pop(0)
 
 while True:
-    #첫번째 주소 불러오기
-    current = download_Queue.pop(0)
 
     #이미지 다운로드
+    if not (current in doneList):
+        print(current)
+        time.sleep(5)
+        doneList.append(current)
 
     #큐 업데이트
     queueFile = open("List.txt", "r")
@@ -17,15 +24,18 @@ while True:
     print(Updated_Queue)
     queueFile.close()
 
-    if len(Updated_Queue) == 0:
-        break
-
     if Updated_Queue[0] == current:
         Updated_Queue.pop(0)
         
         queueFile = open("List.txt", "w")
         for s in Updated_Queue:
-            queueFile.write(s + '\n')
+            queueFile.write(s)
         
         queueFile.close()
 
+    if len(Updated_Queue) == 0:
+        break
+
+    current = Updated_Queue.pop(0)
+
+print("Done!")
