a1a03a1e4bbf20f34f6f848a8b8e3b536f045d8b
KAN-RYU <kanryu2048@unist.ac.kr>
2020-03-12 05:29:09 +0900

    다운로드 큐 시스템 완성

--- Runner.py
+++ Runner.py
@@ -1,2 +1,31 @@
 import Image_Downloader
-import os
+
+queueFile = open("List.txt", "r")
+download_Queue = queueFile.readlines()
+queueFile.close()
+totalNumber = len(download_Queue)
+
+while True:
+    #첫번째 주소 불러오기
+    current = download_Queue.pop(0)
+
+    #이미지 다운로드
+
+    #큐 업데이트
+    queueFile = open("List.txt", "r")
+    Updated_Queue = queueFile.readlines()
+    print(Updated_Queue)
+    queueFile.close()
+
+    if len(Updated_Queue) == 0:
+        break
+
+    if Updated_Queue[0] == current:
+        Updated_Queue.pop(0)
+        
+        queueFile = open("List.txt", "w")
+        for s in Updated_Queue:
+            queueFile.write(s + '\n')
+        
+        queueFile.close()
+
