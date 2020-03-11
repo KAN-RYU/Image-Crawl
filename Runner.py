7ff43cd61fdd154f7741f87fd9c03e8078249f55
KAN-RYU <kanryu2048@unist.ac.kr>
2020-03-12 05:54:00 +0900

    Update Option

--- Runner.py
+++ Runner.py
@@ -1,43 +1,45 @@
 import Image_Downloader
-import time
 
 FILENAME = "List.txt"
+VERBOS = True
 
-queueFile = open(FILENAME, "r")
-download_Queue = queueFile.readlines()
-queueFile.close()
-totalNumber = len(download_Queue)
-doneList = []
-
-#첫번째 주소 불러오기
-current = download_Queue.pop(0)
-
-while True:
-
-    #이미지 다운로드
-    if not (current in doneList):
-        print(current)
-        time.sleep(5)
-        doneList.append(current)
-
-    #큐 업데이트
+if __name__ == "__main__":
     queueFile = open(FILENAME, "r")
-    Updated_Queue = queueFile.readlines()
-    print(Updated_Queue)
+    download_Queue = queueFile.readlines()
     queueFile.close()
 
-    if Updated_Queue[0] == current:
-        Updated_Queue.pop(0)
-        
-        queueFile = open(FILENAME, "w")
-        for s in Updated_Queue:
-            queueFile.write(s)
-        
+    totalNumber = 0
+    doneList = []
+
+    #첫번째 주소 불러오기
+    current = download_Queue.pop(0)
+
+    while True:
+        totalNumber += 1
+
+        #이미지 다운로드
+        if not (current in doneList):
+            Image_Downloader.download_manga(current, VERBOS)
+            doneList.append(current)
+
+        #큐 업데이트
+        queueFile = open(FILENAME, "r")
+        Updated_Queue = queueFile.readlines()
         queueFile.close()
 
-    if len(Updated_Queue) == 0:
-        break
+        if Updated_Queue[0] == current:
+            Updated_Queue.pop(0)
+            
+            queueFile = open(FILENAME, "w")
+            for s in Updated_Queue:
+                queueFile.write(s)
+            
+            queueFile.close()
 
-    current = Updated_Queue.pop(0)
+        if len(Updated_Queue) == 0:
+            break
 
-print("Done!")
+        print("Remaining " + len(Updated_Queue) + " item(s).")
+        current = Updated_Queue.pop(0)
+
+    print("Done! Total: " + totalNumber + " Manga.")
